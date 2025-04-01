ca1a1e48f78361970be80d1b1a6c8928774a4261
BegoCaZa <begoxcz@hotmail.com>
2025-04-01 10:54:56 +0200

    update

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+const box1Element = document.getElementById('box-1');
+const box2Element = document.getElementById('box-2');
+const subtitleElement = document.getElementById('subtitle-1');
+
+console.dir(box1Element);
+console.dir(`Todos los hijos de ${box1Element.id} son: 
+  ${box1Element.children[0].children[0].textContent},
+  ${box1Element.children[0].children[1].textContent},
+  ${box1Element.children[0].children[3].textContent}`);
+
+console.dir(
+  `El item especial es ${box1Element.children[0].children[2].textContent}`
+);
+
+//- Del div con el id box-2 imprime por consola: El textContent y las clases del h2 y el p
+
+console.dir(box2Element);
+console.dir(`El contenido del texto es ${box2Element.textContent}`);
+console.dir(
+  `Las clases de h2 ${box2Element.children[0].children[0].classList}`
+);
+console.dir(
+  `Las clases del p ${box2Element.children[0].children[1].classList}`
+);
+
+// - Del h2 con el id subtitle-1 imprime por consola: Su textContent; la clase del div; el textContent y la clase del p que tiene debajo.
+
+console.dir(`El text content del h2 es ${subtitleElement.textContent}`);
+console.dir(`La clase del div es ${subtitleElement.parentElement.classList}`);
+console.dir(
+  `El text content de p es ${subtitleElement.nextElementSibling.textContent} y la clase es ${subtitleElement.nextElementSibling.classList}`
+);
+
+// - Del h2 con el id subtitle-2 imprime por consola: Su textContent; el textContent y la clase de los dos span que tiene.
